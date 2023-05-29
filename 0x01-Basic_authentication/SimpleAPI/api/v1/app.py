be83cfcb2ba7a0d37b09d065b6454845e79b3b58
David-Inkheart <okoliedave1@gmail.com>
2023-05-29 15:45:12 +0100

    create auth class

--- 0x01-Basic_authentication/SimpleAPI/api/v1/app.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/app.py
@@ -22,14 +22,14 @@ def not_found(error) -> str:
 
 
 @app.errorhandler(401)
-def unauthorized(error) -> str:
+def unauthorized_access(error) -> str:
     """ Unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
-def forbidden(error) -> str:
+def forbidden_action(error) -> str:
     """ Forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
---
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+""" Module of Index Auth class to manage the API authentication
+"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """ Auth class for API authentication
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ require_auth that returns False
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ authorization_header that returns None
+        """
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers['Authorization']
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ current_user
+        """
+        return None
