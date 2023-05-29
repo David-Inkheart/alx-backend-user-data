217ae40e1e79732df1a6638ea542242596671c48
David-Inkheart <okoliedave1@gmail.com>
2023-05-29 13:24:29 +0100

    added Error handler: Forbidden

--- 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
@@ -33,3 +33,12 @@ def unauthorized() -> str:
       - abort with 401 status
     """
     abort(401)
+
+
+@app_views.route('/forbidden/', strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Return:
+      - abort with 403 status
+    """
+    abort(403)
