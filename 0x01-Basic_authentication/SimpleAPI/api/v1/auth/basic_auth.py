2550c24ee86d851b6be07a85a9dc16907135f143
David-Inkheart <okoliedave1@gmail.com>
2023-05-29 17:40:54 +0100

    Basic Auth inheriting from auth class

---
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Inherits from Auth class to manage the API authentication
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """ BasicAuth class for API authentication
+    """
+    pass
