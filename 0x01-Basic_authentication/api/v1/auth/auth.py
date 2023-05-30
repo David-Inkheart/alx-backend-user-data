01276635f1c5ddb2825118b27964d69e40af96a5
David-Inkheart <okoliedave1@gmail.com>
2023-05-30 13:26:20 +0100

    Require auth with stars

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -18,6 +18,10 @@ class Auth():
             path += '/'
         if path in excluded_paths:
             return False
+        for excluded_path in excluded_paths:
+            if excluded_path[-1] == '*':
+                if path.startswith(excluded_path[:-1]):
+                    return False
         return True
 
     def authorization_header(self, request=None) -> str:
