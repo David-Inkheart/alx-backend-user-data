e52f3c7b4674401e2d9be524669435443fe49939
David-Inkheart <okoliedave1@gmail.com>
2023-06-06 15:03:24 +0100

    session_id cookie and login validation

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -11,13 +11,13 @@ AUTH = Auth()
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def home() -> str:
-    """Basic Flask App"""
+    """root route of App"""
     return jsonify({"message": "Bienvenue"})
 
 
 @app.route('/users', methods=['POST'], strict_slashes=False)
 def users() -> str:
-    """Basic Flask App"""
+    """Register a user and return a User object"""
     email = request.form.get('email')
     password = request.form.get('password')
     try:
@@ -25,6 +25,20 @@ def users() -> str:
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """Login route"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie('session_id', session_id)
+        return response
+    else:
+        abort(401)
 
 
 if __name__ == "__main__":
