dd8c1c12c049cb9439c226abb6e18c035110336c
David-Inkheart <okoliedave1@gmail.com>
2023-06-06 15:10:08 +0100

    destroy user session by updatings it's value in user attributes to None

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -41,5 +41,17 @@ def login() -> str:
         abort(401)
 
 
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout() -> str:
+    """Logout route"""
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        AUTH.destroy_session(user.id)
+        return redirect('/')
+    else:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
