266a058df7878a0ca2be7885127a41b1378fe4d9
David-Inkheart <okoliedave1@gmail.com>
2023-06-06 13:14:56 +0100

    register a user on app using Auth class to validate

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,14 +3,28 @@
 """
 
 from flask import Flask, jsonify, request, abort, redirect
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def home() -> str:
     """Basic Flask App"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """Basic Flask App"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
